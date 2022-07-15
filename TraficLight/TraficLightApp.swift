3c3e9c0096e16e4b4cc883be8a575316a35b07b0
leilaaua <leila0777@icloud.com>
2022-07-15 17:26:17 +0300

    Initial Commit

---
+++ TraficLight/TraficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficLightApp.swift
+//  TraficLight
+//
+//  Created by leila on 15.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct TraficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
